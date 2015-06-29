2d0cf310b72befdd2fc22711534971715df28d5f
Kent Oshiro <oshiro@uw.edu>
2015-06-29 15:15:10 -0700

    Initial Commit

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET events listing. */
+router.get('/events', function (req, res) {
+    res.send('respond with a resource');
+});
+
+/* POST event listing. */
+router.get('/events', function (req, res) {
+    res.send('respond with a resource');
+});
+
+/* GET comments listing. */
+router.get('/events', function (req, res) {
+    res.send('respond with a resource');
+});
+
+/* POST comment*/
+router.get('/events', function (req, res) {
+    res.send('respond with a resource');
+});
+
+
+module.exports = router;
