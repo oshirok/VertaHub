5adc57c0b2a9626d6722a5c3fbc4ac9c86bc38f4
Warren <warren@iwarren.com>
2015-07-07 12:02:26 -0700

    Tile Layout - AngularJS Isotope
    everythings ugly. nothing is database related, all hardcoded

--- public/index.js
+++ public/index.js
@@ -7,6 +7,79 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav)
  
     }]);
 
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
+
+// YO YO YO WARREN START HERE ----------------------------------------------------------------------------
+app.controller('myGrid', function($scope){
+	var posts = [{ 
+"timestamp": 1436268134, 
+"name": "Food Trucks: Poke to the Max", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 12pm to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436529600, 
+"time_to": 1436533200, 
+"guest_list": ["John Doe", "Jonathan Rahn", "Lisa Tompa", "Gaetano Pyle"] 
+}, 
+{ 
+"timestamp": 1436268859, 
+"name": "Food Trucks: Circus Burgers", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 11am to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436871600, 
+"time_to": 1436875200, 
+"guest_list": ["Xi Shu", "Lisa Tompa", "Gaetano Pyle"] 
+}, 
+{ 
+"timestamp": 1436268859, 
+"name": "Food Trucks: Circus Burgers", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 11am to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436871600, 
+"time_to": 1436875200, 
+"guest_list": ["Xi Shu", "Lisa Tompa", "Gaetano Pyle"] 
+}, 
+{ 
+"timestamp": 1436268859, 
+"name": "Food Trucks: Circus Burgers", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 11am to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436871600, 
+"time_to": 1436875200, 
+"guest_list": ["Xi Shu", "Lisa Tompa", "Gaetano Pyle"] 
+}, 
+{ 
+"timestamp": 1436268859, 
+"name": "Food Trucks: Circus Burgers", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 11am to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436871600, 
+"time_to": 1436875200, 
+"guest_list": ["Xi Shu", "Lisa Tompa", "Gaetano Pyle"] 
+}, 
+{ 
+"timestamp": 1436268859, 
+"name": "Food Trucks: Circus Burgers", 
+"desc": "Come enjoy lunch with some of your fellow coworkers. Meet at lobby at 11am to walk to the food truck!", 
+"location": "North Creek Parkway", 
+"category": "food", 
+"time_from": 1436871600, 
+"time_to": 1436875200, 
+"guest_list": ["Xi Shu", "Lisa Tompa", "Gaetano Pyle"] 
+}]; 
+	$scope.getPosts = function(){
+		return posts;
+	};
+
+});
+// YO YO YO THIS IS WHERE YOUR GRID ENDS ------------------------------------------------------------------
 
 app.config(function ($mdThemingProvider) {
     
@@ -24,3 +97,18 @@ app.config(function ($mdThemingProvider) {
     .accentPalette('pink')
     .warnPalette('red')
 })
+
+// external js: isotope.pkgd.js
+
+$(document).ready( function() {
+
+  $('.grid').isotope({
+  	layoutMode: 'fitRows',
+    itemSelector: '.grid-item'
+  });
+
+});
+
+
+
+
