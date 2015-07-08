664e61953d0c483ff959b47fbe29cda97906bcc4
Kent Oshiro <oshiro@uw.edu>
2015-07-07 17:47:40 -0700

    got angular material grid working

--- public/index.js
+++ public/index.js
@@ -10,7 +10,13 @@ app.controller('AppCtrl', ['$http', '$scope', '$mdSidenav', '$mdDialog', functio
         $http.get('/api/posts')
         .success(function (data) {
             $scope.post_list = data;
-            console.log(data);
+            
+            console.log(data.length);
+            var colors = ["green", "darkBlue", "purple", "deepBlue", "yellow", "lightPurple", "pink", "red"];
+            for (var i = 0; i < $scope.post_list.length; i++) {
+                $scope.post_list[i].background = colors[Math.floor((Math.random() * colors.length))];
+                console.log($scope.post_list[i].background);
+            }
 			/*for(i in data){
 				console.log(i);
 				i.timestamp = new Date(parseInt(data.timestamp));
@@ -29,8 +35,7 @@ app.controller('AppCtrl', ['$http', '$scope', '$mdSidenav', '$mdDialog', functio
             console.log('broadcast recieved');
             $http.get('/api/posts')
             .success(function (data) {
-                    $scope.post_list = data;
-                    console.log(data);
+                $scope.post_list = data;
 			    /*for(i in data){
 				    console.log(i);
 				    i.timestamp = new Date(parseInt(data.timestamp));
@@ -95,8 +100,14 @@ app.controller('AppCtrl', ['$http', '$scope', '$mdSidenav', '$mdDialog', functio
                 .success(function (data) {
                 // clear the form, allowing the user to send more messages
                 $scope.new_post = {};
+                $scope.post_list = [];
                 $scope.post_list = data;
                 console.log(data);
+                var colors = ["green", "darkBlue", "purple", "deepBlue", "yellow", "lightPurple", "pink", "red"];
+                for (var i = 0; i < $scope.post_list.length; i++) {
+                    $scope.post_list[i].background = colors[Math.floor((Math.random() * colors.length))];
+                    console.log($scope.post_list[i].background);
+                }
                 $mdDialog.hide();
             })
                 .error(function (data) {
@@ -107,7 +118,50 @@ app.controller('AppCtrl', ['$http', '$scope', '$mdSidenav', '$mdDialog', functio
  
     }]);
 
+app.controller('gridListDemoCtrl', function ($scope) {
+    this.tiles = buildGridModel({
+        icon : "avatar:svg-",
+        title: "Svg-",
+        background: ""
+    });
+    function buildGridModel(tileTmpl) {
+        var it, results = [];
+        for (var j = 0; j < 11; j++) {
+            it = angular.extend({}, tileTmpl);
+            it.icon = it.icon + (j + 1);
+            it.title = it.title + (j + 1);
+            it.span = { row : 1, col : 1 };
+            switch (j + 1) {
+                case 1:
+                    it.background = "red";
+                    it.span.row = it.span.col = 2;
+                    break;
+                case 2: it.background = "green"; break;
+                case 3: it.background = "darkBlue"; break;
+                case 4:
+                    it.background = "blue";
+                    it.span.col = 2;
+                    break;
+                case 5:
+                    it.background = "yellow";
+                    it.span.row = it.span.col = 2;
+                    break;
+                case 6: it.background = "pink"; break;
+                case 7: it.background = "darkBlue"; break;
+                case 8: it.background = "purple"; break;
+                case 9: it.background = "deepBlue"; break;
+                case 10: it.background = "lightPurple"; break;
+                case 11: it.background = "yellow"; break;
+            }
+            results.push(it);
+        }
+        return results;
+    }
+})
 
+app.config(function ($mdIconProvider) {
+    $mdIconProvider.iconSet("avatar", 'icons/avatar-icons.svg', 128);
+});
 app.controller('DialogController', function ($http, $scope, $mdDialog) {
     //alert( this.closeDialog );
     //this.closeDialog = $scope.closeDialog;
