cdcca2ec03eca6dfacc94be643af5aa4ae8b1da8
Warren <warren@iwarren.com>
2015-07-08 15:06:14 -0700

    Added an expand post by clicking feature
    Now you can expand posts and see description after clicking

--- public/index.js
+++ public/index.js
@@ -118,46 +118,69 @@ app.controller('AppCtrl', ['$http', '$scope', '$mdSidenav', '$mdDialog', functio
  
     }]);
 
-app.controller('gridListDemoCtrl', function ($scope) {
-    this.tiles = buildGridModel({
-        icon : "avatar:svg-",
-        title: "Svg-",
-        background: ""
-    });
-    function buildGridModel(tileTmpl) {
-        var it, results = [];
-        for (var j = 0; j < 11; j++) {
-            it = angular.extend({}, tileTmpl);
-            it.icon = it.icon + (j + 1);
-            it.title = it.title + (j + 1);
-            it.span = { row : 1, col : 1 };
-            switch (j + 1) {
-                case 1:
-                    it.background = "red";
-                    it.span.row = it.span.col = 2;
-                    break;
-                case 2: it.background = "green"; break;
-                case 3: it.background = "darkBlue"; break;
-                case 4:
-                    it.background = "blue";
-                    it.span.col = 2;
-                    break;
-                case 5:
-                    it.background = "yellow";
-                    it.span.row = it.span.col = 2;
-                    break;
-                case 6: it.background = "pink"; break;
-                case 7: it.background = "darkBlue"; break;
-                case 8: it.background = "purple"; break;
-                case 9: it.background = "deepBlue"; break;
-                case 10: it.background = "lightPurple"; break;
-                case 11: it.background = "yellow"; break;
-            }
-            results.push(it);
-        }
-        return results;
-    }
+app.controller('gridListDemoCtrl', function ($scope, $mdDialog) {
+    this.tiles = [];
 	
+	$scope.showDialog = showDialog;
+	$scope.name;
+	
+	function showDialog($event,name,desc) {
+		$scope.name = name;
+		$scope.desc = desc;
+            var parentEl = angular.element(document.querySelector('md-content'));
+            alert = $mdDialog.alert({
+                parent: parentEl,
+                targetEvent: $event,
+                template:
+                    '<md-dialog aria-label="Sample Dialog">' +
+                    '  <md-content>' +
+                    '    <h1 text-align="center">{{ctrl.name}}</h1> <br>' +
+					'	 <h5>{{ctrl.desc}}</h5>' +
+                    '  <div class="md-actions" >' +
+                    '    <md-button flex ng-click="closeDialog()">' +
+                    '      Close' +
+                    '    </md-button>' +
+                    '  </div>' +
+                    '</md-dialog>',
+                locals: {
+					name: $scope.name,
+					desc: $scope.desc,
+                    closeDialog: $scope.closeDialog,
+                    submitDialog: $scope.submitDialog
+                },
+                bindToController: true,
+                controllerAs: 'ctrl',
+                controller: 'DialogController'
+            });
+            
+            $mdDialog
+              .show(alert)
+              .finally(function () {
+                alert = undefined;
+            });
+        }
+
+        $scope.submitDialog = function () {
+            console.log('submit pressed!');
+            console.log($scope.new_post);
+            $http.post('/api/posts', $scope.new_post)
+                .success(function (data) {
+                // clear the form, allowing the user to send more messages
+                $scope.new_post = {};
+                $scope.post_list = [];
+                $scope.post_list = data;
+                console.log(data);
+                var colors = ["red","blue","green","gray", "orange"];
+                for (var i = 0; i < $scope.post_list.length; i++) {
+                    $scope.post_list[i].background = colors[Math.floor((Math.random() * colors.length))];
+                    console.log($scope.post_list[i].background);
+                }
+                $mdDialog.hide();
+            })
+                .error(function (data) {
+                console.log('Error: ' + data);
+            });
+        };
 })
 
 app.config(function ($mdIconProvider) {
